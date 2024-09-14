cdd43767737b87763be8cf5e0e1a3bc7c2035c96
Adithya369978 <akulaadhi098@gmail.com>
2024-09-14 23:07:17 +0530

    1st changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
 
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
+import { Walletentry } from './pages/wallet';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    // <ConnectionProvider endpoint={endpoint}>
+    // <WalletProvider wallets={wallets} autoConnect>
+    //     <WalletModalProvider>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Walletentry/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      // </WalletModalProvider>
+      //       </WalletProvider>
+      //   </ConnectionProvider>
+    
   )
 }
 
---
+++ src/pages/wallet.jsx
@@ -0,0 +1,10 @@
+export const Walletentry = ()=>{
+    return  <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <button 
+        type="button" 
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+    >
+        Default
+    </button>
+</div>
+}
